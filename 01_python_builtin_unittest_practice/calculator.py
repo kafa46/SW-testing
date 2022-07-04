d0cfbad815616ad6ba606790e87e12f0518bbe10
kafa46 <kafa46@gmail.com>
2022-07-04 16:49:36 +0900

    build calculator class

---
+++ 01_python_builtin_unittest_practice/calculator.py
@@ -0,0 +1,16 @@
+'''calculator class for studying built-in unittest module
+'''
+
+class Calculator:
+    '''Calculator class'''
+    
+    def __init__(self) -> None:
+        pass
+
+    def add(self,):
+        '''adding function'''
+        pass
+
+    def minus(self,):
+        '''substrat function'''
+        pass
